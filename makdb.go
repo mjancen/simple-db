f5392913f6fe0711a45e375278ec8c1245f1a038
Maksim Jancenko <maksimjancenko@gmail.com>
2020-07-19 20:28:29 +0100

    Refactored some code; Added skeleton for SQL statement handling

--- makdb.go
+++ makdb.go
@@ -5,7 +5,51 @@ import (
 	"os"
 	"bufio"
 	"strings"
+	"errors"
 )
+
+const (
+	selectStatement = iota
+	insertStatement
+	invalidStatement
+)
+
+type statementType int
+
+type statement struct {
+	sType statementType
+}
+
+func doMetaCommand(inputCommand string) error {
+	if inputCommand == ".exit" {
+		os.Exit(0)
+		return nil
+	} else {
+		return nil
+	}
+}
+
+func prepareStatement(inputCommand string) (statement, error) {
+	var newStatement statement
+	if strings.HasPrefix(inputCommand, "select") {
+		newStatement.sType = selectStatement
+	} else if strings.HasPrefix(inputCommand, "insert") {
+		newStatement.sType = insertStatement
+	} else {
+		errString := fmt.Sprintf("unrecognised command: %v\n", inputCommand)
+		return statement{sType : invalidStatement}, errors.New(errString)
+	}
+	return newStatement, nil
+}
+
+func executeStatement(st statement) {
+	switch st.sType {
+	case selectStatement:
+		fmt.Println("This is a select statement.")
+	case insertStatement:
+		fmt.Println("This is an insert statement.")
+	}
+}
 
 func main() {
 	stdinScanner := bufio.NewScanner(os.Stdin)
@@ -19,12 +63,22 @@ func main() {
 		}
 		
 		inputText := strings.TrimSpace(stdinScanner.Text())
+		inputText = strings.ToLower(inputText)
 
-		if inputText == ".exit" {
-			os.Exit(0)
-		} else {
-			fmt.Printf("Unrecognised command: %v\n", inputText)
+		if inputText[0] == '.' {
+			err := doMetaCommand(inputText)
+			if err != nil {
+				fmt.Printf("Unrecognised meta-command: %v\n", inputText)
+			}
 		}
+		
+		inputStatement, err := prepareStatement(inputText)
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+		} else {
+			executeStatement(inputStatement)
+		}
+
 	}
 
 	if err := stdinScanner.Err(); err != nil {
